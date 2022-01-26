d63d0a4787e58366d91727037f849cd2ff9e99ff
William Siagian <wsiagian11@gmail.com>
2022-01-26 07:37:28 +0700

    Tugas-2 Uploaded

---
+++ Tugas-2/Main.py
@@ -0,0 +1,25 @@
+import contModule
+
+def menu():
+    print("1. Daftar Kontak")
+    print("2. Tambahkan Kontak")
+    print("3. Keluar")
+
+menu()
+
+option = int(input("Pilih menu: "))
+
+while option != 0:
+    if option == 1:
+        contModule.printList()
+    elif option == 2:
+        contModule.addList()
+    elif option == 3:
+        print("Terima kasih sudah menggunakan program ini!")
+        exit()
+    else:
+        print("Opsi tidak ada.")
+
+    print()
+    menu()
+    option = int(input("Pilih menu: "))
---
+++ Tugas-2/contModule.py
@@ -0,0 +1,15 @@
+def printList():
+    f = open('D:\\Will\\Lectures\\Outside\\AI Indonesia - Basic Python\\Tugas-2\DaftarKontak.txt', 'r')
+    for i in f:
+        print(f.read())
+    f.close()
+
+def addList():
+    f = open('D:\\Will\\Lectures\\Outside\\AI Indonesia - Basic Python\\Tugas-2\DaftarKontak.txt', 'a')
+    strNama = input("Masukkan nama kontak: ")
+    strNum = input("Masukkan nomor kontak: ")
+    f.write("Nama: " + strNama + "\n")
+    f.write("Nomor: " + strNum + "\n\n")
+    f.close()
+
+
